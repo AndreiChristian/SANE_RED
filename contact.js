acab7afb456b4685a6369e52ed725851b0e37429
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-06 09:25:03 +0200

    made contact page responsive

--- contact.js
+++ contact.js
@@ -1,10 +1,8 @@
-// gsap.to(".span", {
-//   duration: 2,
-
-//   ease: "power1.in",
-//   opacity: 0,
-//   repeat: -1,
-//   yoyo: true,
-// });
-
-// let tlMail = gsap.timeline({ repeat: -1 });
+gsap.to("p", {
+  text: "typewriter effect with GSAP 3",
+  ease: "power1.in",
+  duration: 2,
+  repeat: 10,
+  yoyo: true,
+  repeatDelay: 0.4,
+});
